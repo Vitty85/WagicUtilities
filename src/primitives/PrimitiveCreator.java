8c495daa5193d96743620d92b7060c0838bc1813
Vittorio Alfieri <vittorioalfieri@gmail.com>
2021-09-19 14:18:14 +0200

    General improvements for MID, MIC and CC2 sets.

--- src/primitives/PrimitiveCreator.java
+++ src/primitives/PrimitiveCreator.java
@@ -17,7 +17,7 @@ public class PrimitiveCreator {
 
     public static void main(String[] argv) throws IOException {
 
-        String set = "J21";
+        String set = "CC2";
 
         String basePath = "C:\\Program Files (x86)\\Emulatori\\Sony\\PSVita\\Games\\PSP\\Wagic\\WTH 0.23.1\\Res\\sets\\";
         File baseFolder = new File(basePath + "primitives\\");
